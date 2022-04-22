b322b9fa033476279a5bcd4e467132e7550b55ea
blackmanjack <faiz_m17@apps.ipb.ac.id>
2022-04-22 23:44:08 +0700

    add testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,42 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
+import store from "./utils/redux/store";
+import Loginpage from "./pages/Login";
+import { BrowserRouter, Redirect } from "react-router-dom";
+import Home from "./pages/Home";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("Should render login page properly", () => {
+  render(
+    <Provider store={store}>
+      <Loginpage />
+    </Provider>
+  );
+
+  const linkElement = screen.getByText(/Welcome to SpotifyClone/i);
+  const loginBtn = screen.getByText("Login");
+  userEvent.click(loginBtn);
+  expect(loginBtn).toBeInTheDocument();
   expect(linkElement).toBeInTheDocument();
+
+  // await waitFor(() => {
+  //   expect(window.location.pathname).toBe("https://accounts.spotify.com");
+  // });
+  // expect (await window.location.pathname).toBe("https://accounts.spotify.com");
+  // expect(window.location.replace).toEqual(
+  //   "https://accounts.spotify.com/"
+  // );
+});
+
+test("Should render home page properly", async () => {
+  render(
+    <>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </Provider>
+    </>
+  );
+  expect(await screen.findByText("New Release")).toBeInTheDocument();
 });
